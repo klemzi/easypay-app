fa4185f1105568b40af81b5e929b53251e08dce9
Clement Dike <dikeclementu@gmail.com>
2023-04-25 21:22:39 -0600

    test workflow 4

--- easypay-app/server.js
+++ easypay-app/server.js
@@ -1,4 +1,4 @@
-// Simplilearn PG DevOps Project 1 submission
+// Simplilearn PG DevOps Project submission
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
